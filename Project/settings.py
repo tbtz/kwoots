22c232069f50adfefb47f684055fc4196b0a7e2d
Bautz, Tom <tom.bautz@outlook.com>
2019-11-13 17:14:02 +0100

    uniformly naming

--- Project/settings.py
+++ Project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts.apps.AccountsConfig',
+    'Accounts.apps.AccountsConfig',
     'Posts.apps.PostsConfig',
 ]
 
