0d4503ea31c09566fd78c28db0dfd96b10bbfe0e
Bautz, Tom <tom.bautz@outlook.com>
2019-11-13 19:03:19 +0100

    Add function based views, Add delete functionality

--- Posts/urls.py
+++ Posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('create/', views.PostCreateView.as_view(), name='post_create')
+    path('', views.post_list, name='post_list'),
+    path('<int:pk>/', views.post_details, name='post_details'),
+    path('create/', views.create_post, name='create_post')
 ]
--- Posts/views.py
+++ Posts/views.py
@@ -1,28 +1,63 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView
 from .forms import PostForm
 from .models import Post
+from django.shortcuts import redirect, render
 
 
 class PostListView(ListView):
     model = Post
-    context_object_name = 'all_the_posts'
+    context_object_name = 'posts'
     template_name = 'post-list.html'
 
 
-class PostDetailView(DetailView):
+def post_list(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'post-list.html', context)
+
+
+class PostDetailsView(DetailView):
     model = Post
-    context_object_name = 'that_one_post'
-    template_name = 'post-detail.html'
+    context_object_name = 'post'
+    template_name = 'post-details.html'
 
 
-class PostCreateView(CreateView):
+def post_details(request, **kwargs):
+    post_id = kwargs['pk']
+    if request.method == 'POST':
+        Post.objects.filter(id=post_id).delete()
+        return redirect('post_list')
+    else:
+        post = Post.objects.get(id=post_id)
+        context = {'post': post}
+        return render(request, 'post-details.html', context)
+
+
+class CreatePostView(CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'post-create.html'
+    template_name = 'create-post.html'
     success_url = reverse_lazy('post_list')
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        form.instance.user = request.user
+        if form.is_valid():
+            print(form)
+            form.save()
+            return redirect('post_list')
+        else:
+            print(form.errors)
+            return render(request, 'create-post.html', {'form': form})
+
+    else:
+        form = PostForm()
+        context = {'form': form}
+        return render(request, 'create-post.html', context)
